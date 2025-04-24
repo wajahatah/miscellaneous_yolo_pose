9e32d46cfa4073a95d315cce796865f9513b2ae7
wajahatah <wajahat.ahmedhashmi@gmail.com>
2025-04-24 19:09:04 +0500

    modification of read and get pixel from image instead of video

--- pixel.py
+++ pixel.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 # Callback function to capture mouse click events
 def click_event(event, x, y, flags, param):
@@ -12,32 +13,52 @@ def click_event(event, x, y, flags, param):
 
         # Optionally display the click position and pixel value on the frame
         cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)  # Mark the click with a green circle
-        text = f"{pixel_value}"
-        cv2.putText(frame, text, (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+        text = f"x:{x},y:{y}"
+        cv2.putText(frame, text, (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
         cv2.imshow('Frame', frame)
 
 # Load the video
-video_path = "E:/Wajahat/la_chunks/rtsp_61/chunk_27-01-25_13-40.avi"
-cap = cv2.VideoCapture(video_path)
+input_dir = "C:/Users/LAMBDA THETA/Downloads/rtsp_images"
+image_name = "91.jpg"
+image_path = os.path.join(input_dir, image_name)
+image = cv2.imread(image_path)
 
-if not cap.isOpened():
-    print("Error: Could not open video.")
-else:
-    # Read the first frame
-    ret, frame = cap.read()
-    if ret:
-        # Display the frame
-        cv2.imshow('Frame', frame)
-        print("Click on the frame to get pixel values and coordinates.")
+#to get the pixel values from the video frame uncomment the following lines
+# video logic starts here 
+# video_path = "E:/Wajahat/la_chunks/rtsp_61/chunk_27-01-25_13-40.avi"
+# cap = cv2.VideoCapture(video_path)
+# if not cap.isOpened():
+#     print("Error: Could not open video.")
+# else:
+#     # Read the first frame
+#     ret, frame = cap.read()
+#     if ret:
+#         # Display the frame
+#         cv2.imshow('Frame', frame)
+#         print("Click on the frame to get pixel values and coordinates.")
+
+# end of video logic
+
+#image logic starts here
+if image is not None:
         
+    cv2.imshow('Frame', image)
+    print("Click on the image to get pixel values and coordinates.")
+
+    cv2.setMouseCallback('Frame', click_event, image)
+
+    cv2.waitKey(0)
+# logic ends here
+
+#uncomment the following lines to get the pixel values from the video frame
         # Set the mouse callback
-        cv2.setMouseCallback('Frame', click_event, frame)
+#         cv2.setMouseCallback('Frame', click_event, image)
         
-        # Wait for a key press or mouse interaction
-        cv2.waitKey(0)
-    else:
-        print("Error: Could not read the first frame.")
+#         # Wait for a key press or mouse interaction
+#         cv2.waitKey(0)
+#     else:
+#         print("Error: Could not read the first frame.")
 
-# Release resources
-cap.release()
+# # Release resources
+# cap.release()
 cv2.destroyAllWindows()
